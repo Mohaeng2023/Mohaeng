8dd951c7a8b5539e6558f7497e31cd3baceb2c5f
binaryhong <binaryhong@gmail.com>
2023-04-04 03:49:26 +0900

    Feat:결측치 제거를 위한 QueryDsl 관련 설정 추가

--- backend/src/main/java/com/mohaeng/backend/place/repository/PlaceRepository.java
+++ backend/src/main/java/com/mohaeng/backend/place/repository/PlaceRepository.java
@@ -7,12 +7,13 @@ import com.mohaeng.backend.place.dto.PlaceSearchDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom{
+public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom, QuerydslPredicateExecutor<Place> {
     List<Place> findByAddressContainingIgnoreCase(String searchValue);
     Page<PlaceSearchDto> findByNameContainingOrAddressContaining(String name, String address, Pageable pageable);
     Page<PlaceSearchDto> findByNameContaining(String name, Pageable pageable);
