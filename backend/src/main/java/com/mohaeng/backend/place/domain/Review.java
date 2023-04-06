d37d0a511fcf9585c9bb5fdefeac8daee2f62d6c
binaryhong <binaryhong@gmail.com>
2023-04-06 14:48:01 +0900

    Refactor: Review Title제거

--- backend/src/main/java/com/mohaeng/backend/place/domain/Review.java
+++ backend/src/main/java/com/mohaeng/backend/place/domain/Review.java
@@ -5,8 +5,7 @@ import com.mohaeng.backend.common.BaseTimeEntity;
 import com.mohaeng.backend.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Entity
@@ -27,10 +26,38 @@ public class Review extends BaseTimeEntity {
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    private String title;
+//    private String title;
     private String content;
     private int likeCount;
     private double rating;
     private String imgUrl;
 
+    public Review(//String title,
+                  String content, double rating) {
+//        this.title = title;
+        this.content = content;
+        this.rating = rating;
+    }
+
+    public void update(//String title,
+                       String content, double rating, MultipartFile multipartFile) {
+//        this.title = title;
+        this.content = content;
+        this.rating = rating;
+    }
+
+    public Review(Long id, Member member, String content, double rating) {
+        this.id = id;
+        this.member = member;
+        this.content = content;
+        this.rating = rating;
+    }
+
+    public Review(//String title,
+                  String content, double rating, Member member) {
+//        this.title = title;
+        this.content = content;
+        this.rating = rating;
+        this.member = member;
+    }
 }
