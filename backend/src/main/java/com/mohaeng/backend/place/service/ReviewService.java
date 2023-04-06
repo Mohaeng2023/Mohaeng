b601c3af5e350cd32673dbc733b7555d77c12568
binaryhong <binaryhong@gmail.com>
2023-04-06 14:50:32 +0900

    Feat: Review 수정, 삭제, (평균평점, 전체개수) 조회를 위한 ReviewSummary 추가, Title 제거

--- backend/src/main/java/com/mohaeng/backend/place/service/ReviewService.java
+++ backend/src/main/java/com/mohaeng/backend/place/service/ReviewService.java
@@ -5,6 +5,7 @@ import com.mohaeng.backend.member.repository.MemberRepository;
 import com.mohaeng.backend.place.domain.Place;
 import com.mohaeng.backend.place.domain.Review;
 import com.mohaeng.backend.place.dto.request.CreateReviewRequest;
+import com.mohaeng.backend.place.dto.request.UpdateReviewRequest;
 import com.mohaeng.backend.place.dto.response.FindAllReviewResponse;
 import com.mohaeng.backend.place.exception.PlaceNotFoundException;
 import com.mohaeng.backend.place.repository.PlaceRepository;
@@ -15,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
@@ -43,7 +46,7 @@ public class ReviewService {
         Review review = Review.builder()
                 .place(findPlace)
                 .member(findMember)
-                .title(createReviewRequest.getTitle())
+//                .title(createReviewRequest.getTitle())
                 .rating(createReviewRequest.getRating())
                 .content(createReviewRequest.getContent())
                 .build();
@@ -51,5 +54,49 @@ public class ReviewService {
         reviewRepository.save(review);
         findMember.addReview(review);
         findPlace.addReview(review);
+    }
+
+    @Transactional
+    public void updateReview(String email, Long placeId, UpdateReviewRequest updateReviewRequest, MultipartFile multipartFile) {
+        Member findMember = memberRepository.findByEmailAndDeletedDateIsNull(email)
+                .orElseThrow(() -> new IllegalArgumentException("NOT_EXIST_MEMBER"));
+
+        Review review = reviewRepository.findById(placeId)
+                .orElseThrow(() -> new IllegalArgumentException("NOT_EXIST_REVIEW"));
+        review.update(//updateReviewRequest.getTitle(),
+                updateReviewRequest.getContent(), updateReviewRequest.getRating(), updateReviewRequest.getMultipartFile());
+
+        if (!review.getMember().getId().equals(findMember.getId())) {
+            throw new IllegalArgumentException("NOT_AUTHORIZED");
+        }
+        review.update(//updateReviewRequest.getTitle(),
+                updateReviewRequest.getContent(), updateReviewRequest.getRating(),updateReviewRequest.getMultipartFile());
+    }
+
+    @Transactional
+    public void deleteReview(String email, Long placeId) {
+        Member findMember = memberRepository.findByEmailAndDeletedDateIsNull(email)
+                .orElseThrow(() -> new IllegalArgumentException("NOT_EXIST_MEMBER"));
+        Review review = reviewRepository.findById(placeId)
+                .orElseThrow(() -> new IllegalArgumentException("NOT_EXIST_REVIEW"));
+        if (!review.getMember().getId().equals(findMember.getId())) {
+            throw new IllegalArgumentException("NOT_AUTHORIZED");
+        }
+        reviewRepository.delete(review);
+    }
+
+    public Map<String, Object> getReviewSummary(Long placeId) {
+        Place findPlace = placeRepository.findById(placeId)
+                .orElseThrow(() -> new PlaceNotFoundException("NOT_EXIST_PLACE"));
+        long count = findPlace.getReviewList().size();
+        double averageRating = findPlace.getReviewList().stream()
+                .mapToDouble(Review::getRating)
+                .average()
+                .orElse(0);
+        averageRating = Math.round(averageRating * 10) / 10.0;
+        Map<String, Object> result = new ConcurrentHashMap<>();
+        result.put("count", count);
+        result.put("averageRating", averageRating);
+        return result;
     }
 }
